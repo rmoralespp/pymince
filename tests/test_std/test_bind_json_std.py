f5bca78b677ff83ba8d2bce8356dc1bcf2af7fd2
Rolando <rmoralespp@gmail.com>
2022-06-09 14:10:26 +0200

    Added unit tests to valifate bind_json_std decorator

---
+++ tests/test_std/test_bind_json_std.py
@@ -0,0 +1,36 @@
+import io
+import json
+import sys
+import unittest.mock
+
+import pytest
+
+import pymince.std
+
+
+@pytest.fixture()
+def stub_stdin_payload():
+    sys_stdin = sys.stdin
+    initial = {'in': 'value'}
+    sys.stdin = io.TextIOWrapper(buffer=io.BytesIO(json.dumps(initial).encode()))
+    try:
+        yield initial
+    finally:
+        sys.stdin = sys_stdin
+
+
+def test_bind_json_std_with_output(capsys, stub_stdin_payload):
+    expected = {"output": "value"}
+    function = unittest.mock.Mock(return_value=expected)
+    pymince.std.bind_json_std()(function)()
+    captured = capsys.readouterr()
+    function.assert_called_once_with(data=stub_stdin_payload)
+    assert captured.out == json.dumps(expected)
+
+
+def test_bind_json_std_without_output(capsys, stub_stdin_payload):
+    function = unittest.mock.Mock(return_value=None)
+    pymince.std.bind_json_std()(function)()
+    captured = capsys.readouterr()
+    function.assert_called_once_with(data=stub_stdin_payload)
+    assert captured.out == ""
